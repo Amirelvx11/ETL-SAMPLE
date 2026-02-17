4d95b0099768856d72124a504b84478dac9d31a9
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2026-02-17 12:04:58 +0330

    change main, remove type hints and return for main loop (dont need it in any where and we just need final log of etl cycle with addition time duration and start id) & improve error handling.

--- main.py
+++ main.py
@@ -1,47 +1,47 @@
-from backend_toolkit.monitoring.run_tracker import RunTracker
+import time
 from backend_toolkit.logger import get_logger
-from src.config import BATCH_SIZE
 from src.fetch import get_last_inserted_tamper_id, fetch_new_tamper_logs
-from src.insert import transform_tamper_rows, insert_tamper_logs
+from src.transform import transform_tamper_rows
+from src.insert import insert_tamper_logs
+from src.config import BATCH_SIZE
 
 logger = get_logger("main-etl")
 
 
-def tamper_logs_etl() -> int:
-    with RunTracker("device-log-etl") as run:
-        run_id = run.run_id
-
-        last_id = get_last_inserted_tamper_id(run_id)
+def run_etl():
+    try:
+        start_ts = time.monotonic()
+        start_id = get_last_inserted_tamper_id()
         total_inserted = 0
         batches = 0
 
         while True:
-            df = fetch_new_tamper_logs(last_id, run_id)
-
+            df = fetch_new_tamper_logs(start_id)
             if df.empty:
                 break
 
             batches += 1
-            last_id = int(df["TamperLogId"].max())
-
             df = transform_tamper_rows(df)
-            total_inserted += insert_tamper_logs(df, run_id)
-
+            total_inserted += insert_tamper_logs(df)
+            duration = round(time.monotonic() - start_ts, 3)
+            
             if len(df) < BATCH_SIZE:
                 break
 
         logger.info(
             "tamper-log etl finished",
             extra={
-                "run_id": run_id,
+                "inserted": total_inserted,
                 "batches": batches,
-                "rows_inserted": total_inserted,
-                "final_last_tamper_log_id": last_id,
+                "start_tamper_log_id": start_id,
+                "last_tamper_log_id": int(df["TamperLogId"].max()),
+                "duration_sec": duration,
             },
         )
-
-        return total_inserted
+    except Exception as e:
+        logger.critical("main etl cycle error", extra={"error":e})
+        raise
 
 
 if __name__ == "__main__":
-    tamper_logs_etl()
+    run_etl()
